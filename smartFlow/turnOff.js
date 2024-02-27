e4b4d412caf87678fc80fd87008c1fb4530c3de9
Gabriel Cupello <64043219+cupello12@users.noreply.github.com>
2024-02-27 15:12:42 -0400

    Add
    aa

--- smartFlow/turnOff.js
+++
@@ -1,7 +0,0 @@
-const { addKeyword } = require("@bot-whatsapp/bot");
-
-module.exports = addKeyword(["bananaoff"], { sensitive: true })
-    .addAction((_,{globalState}) => {
-        globalState.update({status:false})
-    })
-    .addAnswer("BOT OFF!");
