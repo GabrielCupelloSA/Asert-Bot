7c31eb9e2ea368f00d66aa819fefebcb0ca5b82a
Gabriel Cupello <64043219+cupello12@users.noreply.github.com>
2024-02-27 13:04:47 -0400

    add

--- http/index.js
+++ http/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const app = express();
+const express = require("express"); // Importa el módulo express
+const app = express(); // Crea una instancia de la aplicación express
 
-const PORT = process.env.PORT ?? 4000;
+const PORT = process.env.PORT ?? 4000; // Obtiene el puerto del entorno o utiliza el puerto 4000 por defecto
 
 /**
  * Clase de API REST
@@ -10,20 +10,20 @@ class ServerAPI {
   providerWS;
   providerDB;
   constructor(_providerWS, _providerDB) {
-    this.providerWS = _providerWS;
-    this.providerDB = _providerDB;
+    this.providerWS = _providerWS; // Inicializa el proveedor de WS
+    this.providerDB = _providerDB; // Inicializa el proveedor de DB
   }
 
   start() {
     const middleware = (req, _, next) => {
-      req.ws = this.providerWS
-      req.db = this.providerDB
-      next()
-    }
-    app.use(express.json())
-    app.use("/api", middleware, require("./routes"));
-    app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+      req.ws = this.providerWS; // Agrega el proveedor de WS a la solicitud
+      req.db = this.providerDB; // Agrega el proveedor de DB a la solicitud
+      next();
+    };
+    app.use(express.json()); // Utiliza el middleware para analizar el cuerpo de la solicitud como JSON
+    app.use("/api", middleware, require("./routes")); // Utiliza el middleware y las rutas bajo el prefijo '/api'
+    app.listen(PORT, () => console.log(`http://localhost:${PORT}`)); // Inicia el servidor en el puerto especificado
   }
 }
 
-module.exports = ServerAPI;
+module.exports = ServerAPI; // Exporta la clase ServerAPI
--- middleware/chatwoot.middleware.js
+++ middleware/chatwoot.middleware.js
@@ -1,44 +1,52 @@
 module.exports = async (ctx, options) => {
-  const globalState = options.globalState.getMyState();
-  const inboxId = globalState.inbox_id;
-  const chatwood = options.extensions.chatwood;
-  const checkIsSave = await chatwood.searchByNumber(`${ctx.from}`);
-  const name = ctx?.pushName ?? ctx.ProfileName.split(" ").shift();
+  const globalState = options.globalState.getMyState(); // Obtiene el estado global
+  const inboxId = globalState.inbox_id; // Obtiene el ID del buzón de entrada
+  const chatwood = options.extensions.chatwood; // Obtiene la extensión chatwood
+  const checkIsSave = await chatwood.searchByNumber(`${ctx.from}`); // Busca si el número está guardado
+  const name = ctx?.pushName ?? ctx.ProfileName.split(" ").shift(); // Obtiene el nombre del contacto
+
   if (!checkIsSave.length) {
     const contactSave = await chatwood.createContact({
+      // Crea un contacto si no está guardado
       phone_number: `+${ctx.from}`,
       name: name,
     });
     await options.state.update({
+      // Actualiza el estado con el ID del contacto guardado
       chat_wood_id: contactSave.payload.contact.id ?? checkIsSave[0].id,
     });
   } else {
-    await options.state.update({ chat_wood_id: checkIsSave[0].id });
+    await options.state.update({ chat_wood_id: checkIsSave[0].id }); // Actualiza el estado con el ID del contacto existente
   }
-  const currentState = options.state.getMyState();
+
+  const currentState = options.state.getMyState(); // Obtiene el estado actual
 
   const filterConversation = await chatwood.filterConversation({
+    // Filtra la conversación por número de teléfono
     phone_number: ctx.from,
   });
 
   await options.state.update({
+    // Actualiza el estado con el ID de la conversación
     conversation_id: filterConversation.payload.length
       ? filterConversation.payload[0].id
       : 0,
   });
 
   if (!filterConversation.payload.length) {
+    // Crea una conversación si no existe
     const conversation = await chatwood.createConversation({
       inbox_id: inboxId,
       contact_id: currentState.chat_wood_id,
       phone_number: ctx.from,
     });
 
-    await options.state.update({ conversation_id: conversation.id });
+    await options.state.update({ conversation_id: conversation.id }); // Actualiza el estado con el ID de la conversación creada
   }
+
   //Si tienes asignado agente no continua
   if (filterConversation.payload.length && filterConversation.payload[0].meta) {
-    const assignee = filterConversation.payload[0].meta?.assignee;
-    if (assignee) return await options.endFlow();
+    const assignee = filterConversation.payload[0].meta?.assignee; // Verifica si hay un agente asignado
+    if (assignee) return await options.endFlow(); // Finaliza el flujo si hay un agente asignado
   }
 };
